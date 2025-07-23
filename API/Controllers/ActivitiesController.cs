b8354e86abaa0affcaf1e1dbdc36ca5d384e7ed1
Thilaka S <thilaka.s@xenovex.com>
2025-07-23 15:50:58 +0530

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,27 +1,47 @@
 using System;
+using Application.Activities.Command;
+using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers;
 
-public class ActivitiesController(AppDbContext context) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
 
-    public async Task<ActionResult<List<Activity>>> getActivies()
+    public async Task<ActionResult<List<Activity>>> getActivities()
     {
-        return await context.Activities.ToListAsync();
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> getActivityDetail(string id)
     {
-        var activity = await context.Activities.FindAsync(id);
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+    }
 
-        if (activity == null) return NotFound();
+    [HttpPost]
+    public async Task<ActionResult<string>> createActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
 
-        return activity;
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {
+        await Mediator.Send(new EditActivity.Command { Activity = activity });
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command { Id = id });
+        return Ok();
     }
 }
